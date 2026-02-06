2211b6947f0ec2a8e211dc09481435fe86140ae1
jgranick <jgranick@users.noreply.github.com>
2026-02-06 14:57:09 -0800

    math: Export Vector3

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -4,4 +4,5 @@ export { default as Matrix3Pool } from './Matrix3Pool.js';
 export { default as Rectangle } from './Rectangle.js';
 export { default as RectanglePool } from './RectanglePool.js';
 export { default as Vector2 } from './Vector2.js';
+export { default as Vector3 } from './Vector3.js';
 export { default as Vector4 } from './Vector4.js';
